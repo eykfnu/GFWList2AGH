c4eae8e9895cae6ed11a1ff0e2b124abde095f2f
eykfnu <60003949+eykfnu@users.noreply.github.com>
2023-03-02 11:19:53 +0800

    Update release.sh
    更换dns

--- release.sh
+++ release.sh
@@ -114,18 +114,22 @@ function GenerateRules() {
     case ${software_name} in
         adguardhome)
             domestic_dns=(
+                "tls://233.5.5.5"
+                "tls://1.12.12.12"
                 # "https://dns.alidns.com:443/dns-query"
                 # "https://dns.ipv6dns.com:443/dns-query"
                 # "https://doh.360.cn:443/dns-query"
-                "https://doh.pub:443/dns-query"
+                # "https://doh.pub:443/dns-query"
                 # "tls://dns.alidns.com:853"
                 # "tls://dns.ipv6dns.com:853"
                 # "tls://dot.360.cn:853"
                 # "tls://dot.pub:853"
             )
             foreign_dns=(
+                "tls://1.1.1.1"
+                "tls://8.8.8.8"
                 # "https://dns.google:443/dns-query"
-                "https://dns.opendns.com:443/dns-query"
+                # "https://dns.opendns.com:443/dns-query"
                 # "https://dns11.quad9.net:443/dns-query"
                 # "https://dns64.dns.google:443/dns-query"
                 # "tls://dns.google:853"
